fc0a7d43c7253deace121807a2b6d34b914ee16f
vzavolsky <vzavolsky@ya.ru>
2023-04-23 21:41:38 +0300

    Course 2 - Homework 010 - Maven, libraries Initialisation commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("test"));
+    }
+}
